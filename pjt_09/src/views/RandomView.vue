26c41dfe044da5774db3f59a713a48e4e360c463
10yutae29 <10yutae29@gmail.com>
2022-11-11 17:07:52 +0900

    design

--- pjt_09/src/views/RandomView.vue
+++ pjt_09/src/views/RandomView.vue
@@ -1,13 +1,15 @@
 <template>
-  <div>
-    <button @click="getRandomMovie">PICK</button><br>
-    <div v-if="randomMovie">
-      <h1>{{ randomMovie.title}}</h1>
-      <img :src="`https://image.tmdb.org/t/p/original${randomMovie.poster_path}`" alt="">
+  <div class="pt-5">
+    <div id="randmovie" class="mx-auto">
+      <button class="btn btn-success col-12 mb-3" @click="getRandomMovie">PICK</button><br>
+      <div v-if="randomMovie">
+        <img :src="`https://image.tmdb.org/t/p/original${randomMovie.poster_path}`" alt="">
+        <h3 class="mt-4">{{ randomMovie.title}}</h3>
+      </div>
+      <div v-if="!randomMovie">
+        <img :src="`https://image.tmdb.org/t/p/original${movie.poster_path}`" alt="">
+        <h3 class="mt-4">{{ movie.title}}</h3>
     </div>
-    <div v-if="!randomMovie">
-      <h1>{{ movie.title}}</h1>
-      <img :src="`https://image.tmdb.org/t/p/original${movie.poster_path}`" alt="">
       
     </div>
   </div>
@@ -40,5 +42,12 @@ export default {
 </script>
 
 <style>
+img {
+  width: 500px;
+  height: 500px;
+}
+#randmovie {
+  width: 500px;
+}
 
 </style>
