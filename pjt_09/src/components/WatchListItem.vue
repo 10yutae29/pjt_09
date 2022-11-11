3db0ea1b13c9fd6c7b21010a2a140143ad1cac13
dongind.oct <dongind.oct@gmail.com>
2022-11-11 15:32:43 +0900

    watchList

---
+++ pjt_09/src/components/WatchListItem.vue
@@ -0,0 +1,31 @@
+<template>
+  <div 
+  @click="completed" 
+  :class="{ 'is-completed': this.movie.isCompleted }"
+  >
+    {{ movie.title }}
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'WatchListItem',
+  props: {
+    movie: Object,
+  },
+  methods: {
+    completed() {
+      this.$store.commit('UPDATE_STATUS', this.movie)
+    
+    }
+  }
+}
+</script>
+
+<style>
+.is-completed {
+  text-decoration-line: line-through;
+  color: darkgray;
+}
+
+</style>
--- pjt_09/src/store/index.js
+++ pjt_09/src/store/index.js
@@ -6,12 +6,25 @@ Vue.use(Vuex)
 export default new Vuex.Store({
   state: {
     movieData: null,
+    watchList:[]
   },
   getters: {
   },
   mutations: {
     GET_DATA(state,data){
       state.movieData = data
+    },
+    ADD_WATCH_LIST(state, data) {
+      state.watchList.push(data)
+    },
+    UPDATE_STATUS(state, data) {
+      state.watchList = state.watchList.map((movie) => {
+        if (movie === data) {
+          movie.isCompleted = !movie.isCompleted
+        }
+        return movie
+      })
+
     }
   },
   actions: {
